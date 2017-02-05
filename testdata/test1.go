a66ac1c5ac892f61cd6d14623e5dd81285b54ef9
Vytautas Šaltenis <vytas@rtfb.lt>
2017-02-05 19:15:13 +0200

    Add Fibonacci test case

--- testdata/test1.go
+++ testdata/test1.go
@@ -47,6 +47,25 @@ func multiline(aVeryLongParameterOne string, aVeryLongParameterTwo float64,
 	return "return value", nil
 }
 
+func fibonacci(n int) int {
+	/*
+		@test = {
+			{1, 1},
+			{2, 1},
+			{3, 2},
+			{7, 13},
+			{11, 89},
+		}
+	*/
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 1
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
 func main() {
 	fmt.Println("vim-go")
 }
