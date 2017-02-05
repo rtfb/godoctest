746738f064fe587ef200e6dd45a5e7ec6a6311c9
Vytautas Šaltenis <vytas@rtfb.lt>
2017-02-05 17:29:19 +0200

    Extract func

--- cmd/godoctest/main.go
+++ cmd/godoctest/main.go
@@ -8,19 +8,12 @@ import (
 	"github.com/rtfb/godoctest"
 )
 
-func main() {
-	e := godoctest.NewExtractor()
-	fcs := e.Run("testdata")
-	testCode := godoctest.GenPkgTests(fcs[0])
-	err := ioutil.WriteFile(fcs[0].TestFileName(), []byte(testCode), 0666)
-	if err != nil {
-		panic(err)
-	}
+func runGoTest() {
 	cmd := exec.Command("go", "test")
 	cmd.Dir = "testdata"
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err = cmd.Run()
+	err := cmd.Run()
 	if err != nil {
 		switch typedErr := err.(type) {
 		case *exec.ExitError:
@@ -29,4 +22,15 @@ func main() {
 			panic(err)
 		}
 	}
+}
+
+func main() {
+	e := godoctest.NewExtractor()
+	fcs := e.Run("testdata")
+	testCode := godoctest.GenPkgTests(fcs[0])
+	err := ioutil.WriteFile(fcs[0].TestFileName(), []byte(testCode), 0666)
+	if err != nil {
+		panic(err)
+	}
+	runGoTest()
 }
