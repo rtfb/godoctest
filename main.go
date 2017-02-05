2be8ae400df55dd46f68d6693271455f8fa2dba1
Vytautas Šaltenis <vytas@rtfb.lt>
2017-02-05 12:37:04 +0200

    Move extraction code to extract.go

--- main.go
+++ main.go
@@ -1,114 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"go/ast"
 	"go/parser"
 	"go/token"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 )
-
-type fileComments struct {
-	pkg          *ast.Package
-	fileName     string
-	file         *ast.File
-	comments     map[int]*ast.CommentGroup
-	funcComments map[string]funcData
-}
-
-type funcData struct {
-	declLine    int
-	commentLine int
-	decl        *ast.FuncType
-	comment     *ast.CommentGroup
-}
-
-func containsTestAnnotation(cg *ast.CommentGroup) bool {
-	for _, c := range cg.List {
-		if strings.Contains(c.Text, "@test") {
-			return true
-		}
-	}
-	return false
-}
-
-func extractComments(pkgs map[string]*ast.Package, fs *token.FileSet) []*fileComments {
-	var fcs []*fileComments
-	for _, v := range pkgs {
-		for fk, fv := range v.Files {
-			var fc fileComments
-			fc.comments = map[int]*ast.CommentGroup{}
-			fc.pkg = v
-			fc.fileName = fk
-			fc.file = fv
-			for _, c := range fv.Comments {
-				if !containsTestAnnotation(c) {
-					continue
-				}
-				fc.comments[fs.Position(c.Pos()).Line] = c
-			}
-			fcs = append(fcs, &fc)
-		}
-	}
-	return fcs
-}
-
-func extractFuncs(fcs []*fileComments, fs *token.FileSet) {
-	for _, fc := range fcs {
-		fc.funcComments = map[string]funcData{}
-		for _, d := range fc.file.Decls {
-			switch typedDecl := d.(type) {
-			case *ast.FuncDecl:
-				declLine := fs.Position(d.Pos()).Line
-				endLine := fs.Position(typedDecl.Type.End()).Line
-				_, found := fc.comments[endLine+1]
-				if !found {
-					continue
-				}
-				fc.funcComments[typedDecl.Name.Name] = funcData{
-					declLine:    declLine,
-					commentLine: endLine + 1,
-					decl:        typedDecl.Type,
-					comment:     fc.comments[endLine+1],
-				}
-			}
-		}
-		fc.comments = nil // release for GC
-	}
-}
-
-func cgToStr(cg *ast.CommentGroup) string {
-	var s bytes.Buffer
-	for i, c := range cg.List {
-		if i > 0 {
-			s.WriteByte('\n')
-		}
-		s.WriteString(c.Text)
-	}
-	return s.String()
-}
-
-type templateFuncData struct {
-	FuncName string
-	Hash     string
-}
-
-func prepForTemplate(fcm map[string]funcData) []templateFuncData {
-	var result []templateFuncData
-	i := 0
-	for k := range fcm {
-		i++
-		result = append(result, templateFuncData{
-			FuncName: k,
-			Hash:     strconv.Itoa(i),
-		})
-	}
-	return result
-}
 
 func main() {
 	fs := token.NewFileSet()
